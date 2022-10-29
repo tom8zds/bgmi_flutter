d60750afb7c921b2c025d6c772401f2535d16b14
tom8zds <2407164659@qq.com>
2022-10-29 21:55:15 +0800

    Update logic_test.dart

--- test/logic_test.dart
+++ test/logic_test.dart
@@ -6,15 +6,15 @@ import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("index api", () async {
-    GetSubscribeIndex getSubscribeIndex = GetSubscribeIndex(Dio());
-    List<SubscribeItem> index = await getSubscribeIndex();
-    print(index);
-  });
+//   test("index api", () async {
+//     GetSubscribeIndex getSubscribeIndex = GetSubscribeIndex(Dio());
+//     List<SubscribeItem> index = await getSubscribeIndex();
+//     print(index);
+//   });
 
-  test("calendar api", () async {
-    GetCalendar getCalendar = GetCalendar(Dio());
-    CalendarData data = await getCalendar();
-    print(data.toJson());
-  });
+//   test("calendar api", () async {
+//     GetCalendar getCalendar = GetCalendar(Dio());
+//     CalendarData data = await getCalendar();
+//     print(data.toJson());
+//   });
 }
